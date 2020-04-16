1f02ae81bdd90f2e523784f3c75caa8e8ad18aeb
微笑城 <454493675@qq.com>
2020-04-16 21:03:34 +0800

    开始封装一个全局单例 toast

--- src/app/user/login/login.page.ts
+++ src/app/user/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  userName: string;
-  password: string;
+  userName: string = null;
+  password: string = null;
   // 这里以上是做 变量的
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
 
+  async presentToast(showMasg: string, showDura = 2000) {
+    const toast = await this.toastCtrl.create({
+      message: showMasg,
+      duration: showDura
+    });
+    toast.present();
+  }
 
+  loginApp() {
+    if (this.userName === null) {
+      this.presentToast('用户名不能为空');
+      return;
+    }
+    if (this.password === null) {
+      this.presentToast('密码不能为空');
+      return;
+    }
+    console.log(`用户登录 账户 : ${this.userName} \n 密码: ${this.password}` );
+  }
+
+  gotoRegister() {
+    console.log(`去登录界面`);
+  }
+
+  gotoForgetPassView() {
+    console.log(`请忘记密码界面`);
+  }
 }
